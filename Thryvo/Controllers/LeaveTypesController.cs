edee616ac4acad4b0059070254c2419d714b83f6
Marcus Johnson <johnsonm@stevijackets.net>
2020-03-23 11:22:55 -0500

    Additions: Delete Functionality with Popup instead of Redirect. Revisions: Query Properly

--- Thryvo/Controllers/LeaveTypesController.cs
+++ Thryvo/Controllers/LeaveTypesController.cs
@@ -120,23 +120,43 @@ namespace Thryvo.Controllers
         // GET: LeaveTypes/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var leavetype = _repo.FindById(id);
+            if (leavetype == null)
+            {
+                return NotFound();
+            }
+            var isSuccess = _repo.Delete(leavetype);
+            if (!isSuccess)
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: LeaveTypes/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, LeaveTypeVM model)
         {
             try
             {
-                // TODO: Add delete logic here
+                var leavetype = _repo.FindById(id);
+                if (leavetype == null)
+                {
+                    return NotFound(); 
+                }
+                var isSuccess = _repo.Delete(leavetype);
+                if (!isSuccess)
+                {
+                    return View(model);
+                }
 
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(model);
             }
         }
     }
